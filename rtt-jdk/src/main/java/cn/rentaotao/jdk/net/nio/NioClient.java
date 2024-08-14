5682f16e7d29f81c2a4a6aa2c790fb77ebdf57c5
rtt <1191623980@qq.com>
2024-08-15 00:52:59 +0800

    0815

--- rtt-jdk/src/main/java/cn/rentaotao/jdk/net/nio/NioClient.java
+++ rtt-jdk/src/main/java/cn/rentaotao/jdk/net/nio/NioClient.java
@@ -1,12 +1,12 @@
 package cn.rentaotao.jdk.net.nio;
 
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -23,6 +23,8 @@ public class NioClient {
 
     private SelectionKey register;
 
+    private final ByteBuffer buffer = ByteBuffer.allocate(1024);
+
     public void connect(String host, int port) throws Exception {
         client = SocketChannel.open();
         selector = Selector.open();
@@ -36,25 +38,46 @@ public class NioClient {
             socketChannel.isConnectionPending();    测试SocketChannel是否正在进行连接
             socketChannel.finishConnect();          校验正在进行套接字连接的SocketChannel是否已经完成连接
          */
-        while (!client.isConnected()) {
-        }
+        while (!client.isConnected()) {}
 
         while (true) {
-            try {
-                selector.select();
-                selector.selectedKeys()
+            System.out.println("数据处理线程：" + Thread.currentThread());
+            // 方法阻塞，直到至少有一个事件发生
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            System.out.println(selectionKeys);
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                iterator.remove();
+                if (selectionKey.isReadable()) {
+                    try {
+                        read(selectionKey);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (selectionKey.isWritable()) {
+                    System.out.println("write: " + selectionKey);
+                }
             }
-
-
         }
+    }
 
+    private void read(SelectionKey key) throws Exception {
+        System.out.println("read: " + key);
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        int len;
+        while ((len = socketChannel.read(buffer)) > 0) {
+            System.out.println(new String(buffer.array(), 0, len, StandardCharsets.UTF_8));
+            buffer.clear();
+        }
     }
 
     public static void main(String[] args) throws Exception {
         System.out.println("主线程：" + Thread.currentThread());
         NioClient nioClient = new NioClient();
         nioClient.connect("127.0.0.1", 8587);
-
         while (true) {
             System.out.println("输入：");
             Scanner scanner = new Scanner(System.in);
@@ -62,7 +85,7 @@ public class NioClient {
             System.out.println("发送内容：" + line);
             if (line != null && !line.isEmpty()) {
                 try {
-                    nioClient.client.write(ByteBuffer.wrap(line.getBytes(StandardCharsets.UTF_8))).get();
+                    nioClient.client.write(ByteBuffer.wrap(line.getBytes(StandardCharsets.UTF_8)));
                 } catch (Exception e) {
                     try {
                         nioClient.client.close();
--- rtt-jdk/src/main/java/cn/rentaotao/jdk/net/nio/NioServer.java
+++ rtt-jdk/src/main/java/cn/rentaotao/jdk/net/nio/NioServer.java
@@ -8,17 +8,23 @@ import java.net.ServerSocket;
 import java.net.SocketOption;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * https://blog.csdn.net/anlian523/article/details/105020210
+ *
  * @author rtt
  * @date 2024/8/13 16:02
  */
 public class NioServer {
-
+    /**
+     * ServerSocketChannel 不具备读写数据的能力，和客户端交互是通过 SocketChannel 实现的.
+     */
     private ServerSocketChannel server;
 
     private final ConcurrentHashMap<String, ConnectionHolder<SocketChannel>> cache = new ConcurrentHashMap<>();
@@ -41,69 +47,85 @@ public class NioServer {
         server.configureBlocking(false);
         server.bind(new InetSocketAddress(port));
         selector = Selector.open();
-        register = server.register(selector, SelectionKey.OP_ACCEPT | SelectionKey.OP_READ);
+        register = server.register(selector, SelectionKey.OP_ACCEPT);
         System.out.println(register);
         while (true) {
             System.out.println("数据处理线程：" + Thread.currentThread());
             // 方法阻塞，直到至少有一个事件发生
-            selector.select();
+            if (selector.select() == 0) {
+                System.out.println(0);
+                continue;
+            }
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
-            System.out.println(selectionKeys);
             while (iterator.hasNext()) {
                 SelectionKey selectionKey = iterator.next();
+                System.out.println(selectionKey);
                 iterator.remove();
                 if (selectionKey.isAcceptable()) {
+                    System.out.println("accept: " + selectionKey);
                     try {
                         accept(selectionKey);
                     } catch (Exception e) {
                         e.printStackTrace();
-                        iterator.remove();
-                        continue;
                     }
                 }
                 if (selectionKey.isReadable()) {
+                    System.out.println("read: " + selectionKey);
                     try {
                         read(selectionKey);
                     } catch (Exception e) {
                         e.printStackTrace();
-                        iterator.remove();
-                        continue;
                     }
                 }
                 if (selectionKey.isWritable()) {
                     System.out.println("write: " + selectionKey);
+                }
+                if (selectionKey.isConnectable()) {
+                    System.out.println("connect: " + selectionKey);
                 }
             }
         }
     }
 
     private void accept(SelectionKey key) throws IOException {
-        System.out.println("accept: " + key);
         ConnectionHolder<SocketChannel> holder = new ConnectionHolder<>();
         SocketChannel socketChannel = (SocketChannel) key.channel();
+        socketChannel.configureBlocking(false);
         holder.setConnection(socketChannel);
         holder.setId(UUID.randomUUID().toString());
         long mills = System.currentTimeMillis();
         holder.setConnTime(mills);
         holder.setLastActive(mills);
         try {
-            InetSocketAddress remoteAddress = (InetSocketAddress)conn.getRemoteAddress();
+            InetSocketAddress remoteAddress = (InetSocketAddress) socketChannel.getRemoteAddress();
             String hostAddress = remoteAddress.getAddress().getHostAddress();
             holder.setRemoteAddr(hostAddress);
             cache.put(holder.getId(), holder);
         } finally {
             key.channel().close();
         }
+        // 将客户端连接注册到 selector
+        socketChannel.register(selector, SelectionKey.OP_READ, holder);
         System.out.println("接受连接：" + holder.getRemoteAddr());
     }
 
-    private void read(SelectionKey key) throws Exception{
+    private void read(SelectionKey key) throws Exception {
         SocketChannel socketChannel = (SocketChannel) key.channel();
-        socketChannel.read(buffer, socketChannel.shutdownInput());
+        int len;
+        while ((len = socketChannel.read(buffer)) > 0) {
+            System.out.println(new String(buffer.array(), 0, len, StandardCharsets.UTF_8));
+            buffer.clear();
+        }
+        @SuppressWarnings("unchecked")
+        ConnectionHolder<SocketChannel> attachment = (ConnectionHolder<SocketChannel>) key.attachment();
+        attachment.setLastActive(System.currentTimeMillis());
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        System.out.println("主线程：" + Thread.currentThread());
+        NioServer nioServer = new NioServer(8587);
+        nioServer.start();
+        System.out.println(1);
     }
 }
